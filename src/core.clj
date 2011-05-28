9671c6e086200a44c9549b310ee19555683d3167
Jason Jackson <jason@cvk.ca>
2011-05-28 03:22:13 -0400

    working version of hangman algo

--- src/core.clj
+++ src/core.clj
@@ -2,29 +2,43 @@
   (:use [util]
         [pallet thread-expr]
         [clojure.contrib
-         [seq-utils :only [indexed]]
-         [pprint :only [pprint-map]]]
+         [seq-utils :only [indexed]]]
         [clojure.java io]))
 
-(def d (filter not-empty
-               (map #(->> % (re-matches #"\d+\s([a-z]+).+")
-                          (drop 1) (first))
-                    (line-seq (reader "dict.txt")))))
-
-(def d$ (take 200 d))
-
-(def len->dict (group-by count d$))
-
-(defn bits-to-int [bits] (-> (int 0) (for-> [[k v] (indexed bits)]
-                                            (when-> v (bit-set k)))))
-
 (def bit-size 31)
+(def alpha (map char (range (int \a) (+ (int \z) 1))))
+
+(defrecord DictEntry [word freq])
+
+
+(defn lazy-dict! []
+  (line-seq (reader "dict.txt")))
+
+(defnl parsed-dict [lazy-dict]
+  parsed-&-normal
+  :where
+  [parsed (filter not-empty
+                  (map #(->> % (re-matches #"(\d+)\s([a-z]+).+") (drop 1))
+                       lazy-dict))
+
+   record-fmt (map (fn [[a b]] (DictEntry. b (Integer/parseInt a))) parsed)
+
+   max-freq (:freq (first record-fmt))
+   normalize-freq #(float (/ % max-freq))
+
+   parsed-&-normal (map #(update-in % [:freq] normalize-freq) record-fmt)])
+
+(defn meta-dict [lazy-dict trim-size]
+  (group-by #(count (:word %)) (take trim-size (parsed-dict lazy-dict))))
+
+(defn bits-to-int [bits]
+  (-> (int 0) (for-> [[k v] (indexed bits)]
+                     (when-> v (bit-set k)))))
 
 (defnl to-bit-array [is-bit-set?]
   (int-array (map bits-to-int seq-32-bits))
   
-  :where [
-          bit->idx #(int (Math/floor (/ % bit-size)))
+  :where [bit->idx #(int (Math/floor (/ % bit-size)))
           idx->bit #(* % bit-size)
 
 
@@ -33,17 +47,25 @@
           asize (+ 1 (bit->idx (count is-bit-set?)))])
 
 (defnl build-index [dict letter pos]
-  (to-bit-array (map #(= (nth % pos) letter) dict)))
+  (to-bit-array (map #(= (nth (:word %) pos) letter) dict)))
 
+(defnl build-meta-index [meta-dict]
+  (len->a->pos->index)
+  
+  :where [pos->index (fn [dict len a]
+                       (into {} (for [p (range len)]
+                                  [p (build-index dict a p)])))
 
-(def alpha (map char (range (int \a) (+ (int \z) 1))))
+          a->pos->index (fn [dict len]
+                          (into {} (for [a alpha]
+                                     [a (pos->index dict len a)])))
+          
+          len->a->pos->index (fn [] (into {} (map
+                                             (fn [[len dict]]
+                                               [len (a->pos->index dict len)])
+                                             meta-dict)))])
 
-#_ (def agg
-     (into {} (for [[len, dict] len->dict]
-                [len (into {} (for [a alpha]
-                                [a (into {} (for [p (range len)]
-                                              [p (build-index dict a p)]))]))])))
-(defnl and-arrays [& as]
+(defnl and-arrays [as]
   (doseq [a as]
     (doseq [[idx, elem] (indexed a)]
       (aset ret idx (bit-and (aget ret idx) (aget a idx)))))
@@ -53,7 +75,68 @@
           ret (int-array sz max-int)])
 
 
+(defnl index-unique [index cnst]
+  (and-arrays partial-cnst-ba)
+  :where [partial-cnst-ba (for [[pos letter] cnst]
+                            ((index letter) pos))])
 
+(defnl bit-array->idxs [ba]
+  (map (comp int first) (filter #(let [[a b] %] b) (indexed expanded)))
+  :where [expanded (for [i (range (alength ba)) j (range bit-size)]
+                     (bit-test (aget ba i) j))])
+
+(defnl ba->words [dict ba]
+  (map dict idxs)
+  :where [idxs (bit-array->idxs ba)])
+
+(defnl extract-unbound-chars [word cnstr]
+  (->> (indexed word)
+       (filter-bound-chars)
+       (remove-indices))
+  :where
+  [bound-pos? (set (keys cnstr))
+   filter-bound-chars #(filter (fn [[idx ch]] (not (bound-pos? idx))) %)
+   remove-indices #(map (fn [[_ ch]] ch) %)])
+
+(defnl dict-to-ch-freq [dict cnstr]
+  (into (sorted-map-by cmp) merged)
+  :where
+  [unmerged (for [d dict]
+              (zipmap (extract-unbound-chars (:word d) cnstr)
+                      (repeat (:freq d))))
+   merged (apply merge-with + unmerged)
+   cmp #(- (compare (merged %1) (merged %2)))])
+
+(defn main []
+  (println "starting")
+  (def lazy-dict (lazy-dict!))
+  (println "building meta-dict")
+  (def meta-dict$ (meta-dict lazy-dict 1000))
+  (println "building meta-index")
+  (def meta-index$ (build-meta-index meta-dict$))
+
+  (do
+    (def len 5)
+    (def index$ (meta-index$ len))
+    (def dict$ (meta-dict$ len))
+    (println "uniquing")
+    (def cnstr {0 \h 1 \e})
+    (def ba (index-unique index$ cnstr))
+    (println "expanding and mapping to words:")
+    (def words$ (ba->words dict$ ba))
+    (println "calculating char frequencies:")
+    (println (take 5 (dict-to-ch-freq words$ cnstr)))
+    )
+  )
+
+(defn -main []
+  (time (main)))
+
+#_ (def indices (build-meta-index))
+#_ (def ba (index-unique indices 3 {0 \h 1 \e} ))
+#_ (ba->words 3 ba)
+#_ (def len 3)
+#_ (def ba-1 ret)
 
 
 #_ (let [letter \o
--- test/test/core.clj
+++ test/test/core.clj
@@ -1,24 +1,25 @@
 (ns test.core
   (:use
    [core]
+   [pallet.thread-expr]
    [clojure.test]))
 
 
-(deftest array-and
-  (is (= [0 0] (seq (and-arrays (int-array [0 0])
-                                (int-array [0 0])))))
-  (is (= [1 1] (seq (and-arrays (int-array [1 1])
-                                (int-array [1 1])))))
-  (is (= [3 3] (seq (and-arrays (int-array [3 3])
-                                (int-array [3 3])))))
-  (is (= [0 1] (seq (and-arrays (int-array [3 7])
-                                (int-array [0 1])))))
-  (is (= [1 1] (seq (and-arrays (int-array [3 7])
-                                (int-array [1 1])))))
-  (is (= [1 3] (seq (and-arrays (int-array [3 7])
-                                (int-array [1 3])
-                                (int-array [3 3]))))))
+(deftest test-array-and
+  (is (= [1 1] (seq (and-arrays [(int-array [3 7])
+                                 (int-array [1 1])]))))
+  (is (= [1 3] (seq (and-arrays [(int-array [3 7])
+                                 (int-array [1 3])
+                                 (int-array [3 3])])))))
 
-(deftest bits-to-ints
+(deftest test-bits-to-ints
   (is (= (bits-to-int (repeat 31 true)) 2147483647)))
+
+#_ (deftest test-cnst-dict
+  (is (= (seq (cnst-dict agg 3 {0 \o})) (((agg 3) \o) 0))))
+
+
+
+
+
 
